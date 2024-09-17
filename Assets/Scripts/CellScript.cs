c56fa241fd04463185170ec187d634394ec3e5a9
steph1saurus <162734991+steph1saurus@users.noreply.github.com>
2024-09-17 14:32:31 -0400

    new cell script

---
+++ Assets/Scripts/CellScript.cs
@@ -0,0 +1,21 @@
+
+using UnityEngine;
+
+public struct Cell
+
+{
+//define classification of cells
+public enum Type
+    {
+        Empty,
+        Mine,
+        Number,
+    }
+    public Vector3Int position; //where the cell is on the "board" with XY coordinates
+    public Type type;
+    public int number; //what number is displayed
+    public bool revealed; //if cell has been revealed or not clicked yet
+    public bool flagged; //if flag is placed on cell
+    public bool explode; //if cell is a mine
+
+}
